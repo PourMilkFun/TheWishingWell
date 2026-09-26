d1f6e1fa9c8643cd1dbf482169ff4d508d3c7a5e
PourMilkFun <pourmilkfun@users.noreply.github.com>
2026-09-25 21:13:35 -0400

    Restore KayKay site password gate
    Soft lock the site again for now; intro still plays before the unlock form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { SolanaProvider } from './wallet/SolanaProvider'
 import { Layout } from './components/Layout'
-import { SiteIntro, shouldShowIntro } from './components/SiteIntro'
+import { SitePasswordGate } from './components/SitePasswordGate'
 import { Home } from './pages/Home'
 import { Launches } from './pages/Launches'
 import { Launch } from './pages/Launch'
@@ -11,27 +10,23 @@ import { Mechanics } from './pages/Mechanics'
 import { Docs, DocsIndexRedirect } from './pages/Docs'
 
 export default function App() {
-  const [introDone, setIntroDone] = useState(() => !shouldShowIntro())
-
-  if (!introDone) {
-    return <SiteIntro onComplete={() => setIntroDone(true)} />
-  }
-
   return (
-    <SolanaProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="launches" element={<Launches />} />
-            <Route path="launch" element={<Launch />} />
-            <Route path="coin/:id" element={<Inspect />} />
-            <Route path="mechanics" element={<Mechanics />} />
-            <Route path="docs" element={<DocsIndexRedirect />} />
-            <Route path="docs/:slug" element={<Docs />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </SolanaProvider>
+    <SitePasswordGate>
+      <SolanaProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="launches" element={<Launches />} />
+              <Route path="launch" element={<Launch />} />
+              <Route path="coin/:id" element={<Inspect />} />
+              <Route path="mechanics" element={<Mechanics />} />
+              <Route path="docs" element={<DocsIndexRedirect />} />
+              <Route path="docs/:slug" element={<Docs />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </SolanaProvider>
+    </SitePasswordGate>
   )
 }
